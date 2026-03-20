be1d61bfe8448b537aae76f98cfad170c2d91cb4
latttchc <iam.yuk2882@gmail.com>
2026-03-20 21:44:07 +0900

    feat: JotaiProviderをAuthBootstrapに置き換え、認証機能を統合

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { PropsWithChildren } from 'react';
 
-import { JotaiProvider } from '@/components/jotai-provider';
 import { ModalProvider } from '@/components/modal-provider';
 import { Toaster } from '@/components/ui/sonner';
 import { siteConfig } from '@/config';
+import { AuthBootstrap } from '@/features/auth/components/auth-bootstrap';
 
 import './globals.css';
 
@@ -19,12 +19,11 @@ const RootLayout = ({ children }: Readonly<PropsWithChildren>) => {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        <JotaiProvider>
+        <AuthBootstrap>
           <Toaster theme="light" richColors closeButton />
           <ModalProvider />
-
           {children}
-        </JotaiProvider>
+        </AuthBootstrap>
       </body>
     </html>
   );
