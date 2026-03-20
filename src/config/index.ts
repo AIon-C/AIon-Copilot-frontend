4b638cdbc4bc9761bbd9c0b13440e4d4994a7c76
mizzzuno <mizz22@outlook.jp>
2026-03-21 04:24:47 +0900

    chamge site name

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
 
 export const siteConfig: Metadata = {
-  title: 'Slack Clone',
+  title: 'AIon Copilot',
   description:
-    'Collaborate with your team using real-time messaging, rich text editing, and emoji support in this Slack-like app built with Next.js and Shadcn UI.',
+    'AIon Copilot is an AI-powered assistant designed to help developers build a Slack clone application. It provides explanations, and guidance throughout the development process, making it easier for developers to create a feature-rich and user-friendly Slack clone.',
   keywords: [
     'reactjs',
     'nextjs',
